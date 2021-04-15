a5ac37845bbaca23b6bd3400d4bb3a42bd8c64bb
mertwithamouth <34316968+MertWithMouth@users.noreply.github.com>
2021-04-15 17:27:05 +0300

    Website Project with Flask (not finished)
    It is a dumb as hell program. Caches creating problem for CSS and it doesn't worth my time. There are better ways to do this.

--- mapping/webmap.py
+++ mapping/webmap.py
@@ -1,6 +1,5 @@
 import folium
 import pandas
-import random
 
 map=folium.Map(location=[40.76, -73.98], zoom_start=15,tiles="Stamen Terrain")
 
@@ -22,17 +21,26 @@ def color(high):
     else:
         return "red"
 
-fg=folium.FeatureGroup(name="My Map")
+fgv=folium.FeatureGroup(name="Volcanoes")
 
 def creating_markers(data,coordinates):
     for i in range(len(data.index)):
-        fg.add_child(folium.CircleMarker(location=coordinates[i],
+        fgv.add_child(folium.CircleMarker(location=coordinates[i],
         popup="Name= %s \n Status= %s" %(data["NAME"][i], data["STATUS"][i]), 
         fill_color=color(data["ELEV"][i]), color=color(data["ELEV"][i]), fill_opacity=0.7))
-    return fg
+    return fgv
+
+
 
 creating_markers(df,coordinates)
 
-map.add_child(fg)
+fgp=folium.FeatureGroup(name="Population")
 
+fgp.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(),
+style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005']<10000000
+else "orange" if 10000000 <= x['properties']['POP2005'] <20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
